d2079cbe3d7cc423cdaed72d72722bdfac5c9ddb
Jacky <jackyandparis@gmail.com>
2020-03-07 21:00:26 -0800

    add method to process form submission and changes

--- src/components/customer/Edit.tsx
+++ src/components/customer/Edit.tsx
@@ -31,12 +31,34 @@ class EditCustomer extends React.Component<RouteComponentProps<any>, IFormState>
     }
 
     // check network request from client side to match customer with their data in db
-    // customers matched by id
+    // customers matched by id then display info on form
     public componentDidMount(): void {
         axios.get(`http://localhost:5000/customers/${this.state.id}`).then(data => {
             this.setState({ customer: data.data });
         })
     }
     
+    // add method to process submission and handle changes to values in fields
+    // use Promise<void> for future-like object as placeholder for async api result, object in ready state
+    private processFormSubmission = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
+        e.preventDefault();
+        this.setState({ loading: true });
+        axios.patch(`http://localhost:5000/customers/${this.state.id}`, this.state.values).then(data => {
+            this.setState({ submitSuccess: true, loading: false })
+            setTimeout(() => {
+                this.props.history.push('/');
+            }, 1500)
+        })
+    }
+    private setValues = (values: IValues) => {
+        this.setState({ values: { ...this.state.values, ...values } });
+    }
+
+    private handleInputChanges = (e:React.FormEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        this.setValues({ [e.currentTarget.id]: e.currentTarget.value})
+    }
+
+    // add method to render the Edit component
 }
 export default withRouter(EditCustomer)
