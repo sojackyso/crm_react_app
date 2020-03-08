c48f1160e8a87e2ef8c0adcba3486fa330b0c4df
Jacky <jackyandparis@gmail.com>
2020-03-07 22:13:56 -0800

    add homepage to display array of customers as editable list

---
+++ src/components/Home.tsx
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import { Link, RouteComponentProps } from 'react-router-dom';
+import axios from 'axios';
+
+interface IState {
+    customers: any[];
+}
+
+export default class Home extends React.Component<RouteComponentProps, IState> {
+    constructor(props: RouteComponentProps) {
+        super(props);
+        this.state = { customers: [] }
+    }
+    // fetch list of customers and update homepage with it
+    public componentDidMount(): void {
+        axios.get(`http://localhost:5000/customers`).then(data => {
+            this.setState({ customers: data.data })
+        })
+    }
+    // customer matched in db by id, accept id as parameter and delete details of customer with that id
+    public deleteCustomer(id: number) {
+        axios.delete(`http://localhost:5000/customers/${id}`).then(data => {
+            const index = this.state.customers.findIndex(customer => customer.id === id);
+            this.state.customers.splice(index, 1);
+            this.props.history.push('/');
+        })
+    }
+
+    // add render() method to display table with list of customers
+    public render() {
+        const customers = this.state.customers;
+        return (
+            <div>
+                {/* if len is zero, then no customer id found. */}
+                {customers.length === 0 && (
+                    <div className="text-center">
+                        <h2>No customer found at the moment</h2>
+                    </div>
+                )}
+
+                <div className="container">
+                    <div className="row">
+                        <table className="table table-bordered">
+                            <thead className="thead-light">
+                                <tr>
+                                    <th scope="col">Firstname</th>
+                                    <th scope="col">Lastname</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Phone</th>
+                                    <th scope="col">Address</th>
+                                    <th scope="col">Description</th>
+                                    <th scope="col">Actions</th>
+                                </tr>
+                            </thead>
+                            {/* customers is an array of each customer matched by customer.id key. Loop over array and display details of each customer in list of customers. */}
+                            <tbody>
+                                {customers && customers.map(customer =>
+                                    <tr key={customer.id}>
+                                        <td>{customer.first_name}</td>
+                                        <td>{customer.last_name}</td>
+                                        <td>{customer.email}</td>
+                                        <td>{customer.phone}</td>
+                                        <td>{customer.address}</td>
+                                        <td>{customer.description}</td>
+                                        <td>
+                                            {/* add buttons to edit or delete customer data */}
+                                            <div className="d-flex justify-content-between align-items-center">
+                                                <div className="btn-group" style={{ marginBottom: "20px" }}>
+                                                    <Link to={`edit/${customer.id}`} className="btn btn-sm btn-outline-secondary">Edit Customer </Link>
+                                                    <button className="btn btn-sm btn-outline-secondary" onClick={() => this.deleteCustomer(customer.id)}>Delete Customer</button>
+                                                </div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                )}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
