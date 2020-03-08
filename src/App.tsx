92f9fb912a4df27792a24cd23f274da3a4033967
Jacky <jackyandparis@gmail.com>
2020-03-07 22:40:27 -0800

    import reusable components into app file and create render method

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
+// import components from react-router-dom.
+import { Switch, Route, withRouter, RouteComponentProps, Link } from 'react-router-dom';
+// import reusable components for creating, editing, and viewing customer details.
+import Home from './components/Home';
+import Create from './components/customer/Create';
+import EditCustomer from './components/customer/Edit';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component<RouteComponentProps<any>> {
+  public render() {
+    return (
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to={'/'}> Home </Link>
+            </li>
+            <li>
+              <Link to={'/create'}> Create New Customer </Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path={'/'} exact component={Home} />
+          <Route path={'/create'} exact component={Create} />
+          {/* find customer to edit by customer.id */}
+          <Route path={'/edit/:id'} exact component={EditCustomer} />
+        </Switch>
+      </div>
+    );
+  }
 }
-
-export default App;
+// withRouter component passes closest route's match, current location, and history objs as props to app component.
+export default withRouter(App);
